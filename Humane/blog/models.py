43e90d1e9c89c83b231c0b76ac29a60aa9543b8c
ra2302 <ramneek2302@gmail.com>
2020-05-12 19:23:10 +0530

    Added basic file structure

--- Humane/blog/models.py
+++ Humane/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=75)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add= True)
+
+
+def __str__(self):
+    return self.title
